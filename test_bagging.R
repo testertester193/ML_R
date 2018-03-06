35bc1aeb21fd97a5a7d2ccd65e882168fb621ec7
kvinlazy <kevinnaik@gmail.com>
2018-03-06 10:08:40 +0530

    update

---
+++ test_bagging.R
@@ -0,0 +1,20 @@
+credit <- read.csv("credit.csv")
+library(ipred)
+set.seed(300)
+mybag <- bagging(default ~ ., data = credit, nbagg = 25)
+credit_pred <- predict(mybag, credit)
+table(credit_pred, credit$default)
+library(caret)
+set.seed(300)
+ctrl <- trainControl(method = "cv", number = 10)
+train(default ~ ., data = credit, method = "treebag",
+        trControl = ctrl)
+str(svmBag)
+svmBag$fit
+bagctrl <- bagControl(fit = svmBag$fit,
+                      predict = svmBag$pred,
+                      aggregate = svmBag$aggregate)
+set.seed(300)
+svmbag <- train(default ~ ., data = credit, "bag",
+                  trControl = ctrl, bagControl = bagctrl)
+svmbag
